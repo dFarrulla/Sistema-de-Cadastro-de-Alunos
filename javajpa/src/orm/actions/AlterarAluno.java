63c06fc72d25ea36201a81e6bb38bf6d2db85965
Daniel Farrulla <104379290+dFarrulla@users.noreply.github.com>
2023-06-30 23:44:07 -0300

    Update AlterarAluno.java
     o usuário será solicitado a fornecer o ID do aluno que deseja alterar os dados.

--- javajpa/src/orm/actions/AlterarAluno.java
+++ javajpa/src/orm/actions/AlterarAluno.java
@@ -1,7 +1,6 @@
 package orm.actions;
 
 import java.util.Scanner;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -10,38 +9,66 @@ import orm.modelo.Aluno;
 public class AlterarAluno {
 
     public static void main(String[] args) {
-    	
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("alunos");
-        EntityManager manager = factory.createEntityManager();
+        try (Scanner input = new Scanner(System.in)) {
+            EntityManagerFactory factory = Persistence.createEntityManagerFactory("alunos");
+            EntityManager manager = factory.createEntityManager();
 
-        // Identificador do aluno que será alterado
-        long alunoId = 1; // Substitua 1 pelo ID do aluno que deseja alterar
+            System.out.print("Forneça o ID do aluno para alterar os dados: ");
+            Long alunoId = input.nextLong();
 
-        // Consulta o aluno pelo ID
-        Aluno aluno = manager.find(Aluno.class, alunoId);
+            Aluno aluno = manager.find(Aluno.class, alunoId);
 
-        if (aluno != null) {
-            // Altera os dados do aluno
-            aluno.setNome("Novo Nome");
-            aluno.setEmail("novoemail@example.com");
-            aluno.setCpf("111.222.333-44");
-            // Atualize os demais atributos conforme necessário
+            if (aluno != null) {
+                System.out.println("Dados atuais do aluno:");
+                exibirDadosAluno(aluno);
 
-            // Iniciar uma transação
-            manager.getTransaction().begin();
+                System.out.println("\nDigite os novos dados:");
 
-            // Atualiza o aluno no banco de dados
-            manager.merge(aluno);
+                System.out.print("Nome: ");
+                input.nextLine(); // Limpa o buffer
+                String nome = input.nextLine();
+                aluno.setNome(nome);
 
-            // Commit da transação
-            manager.getTransaction().commit();
+                System.out.print("E-mail: ");
+                String email = input.nextLine();
+                aluno.setEmail(email);
 
-            System.out.println("Aluno alterado com sucesso!");
-        } else {
-            System.out.println("Aluno não encontrado!");
+                System.out.print("CPF: ");
+                String cpf = input.nextLine();
+                aluno.setCpf(cpf);
+
+                System.out.print("Data de Nascimento: ");
+                String dataNascimento = input.nextLine();
+                aluno.setDataNascimento(dataNascimento);
+
+                System.out.print("Naturalidade: ");
+                String naturalidade = input.nextLine();
+                aluno.setNaturalidade(naturalidade);
+
+                System.out.print("Endereço: ");
+                String endereco = input.nextLine();
+                aluno.setEndereco(endereco);
+
+                manager.getTransaction().begin();
+                manager.merge(aluno); // Atualiza o aluno no banco de dados
+                manager.getTransaction().commit();
+                System.out.println("Aluno atualizado com sucesso!");
+            } else {
+                System.out.println("Aluno não encontrado!");
+            }
+
+            manager.close();
+            factory.close();
         }
+    }
 
-        manager.close();
-        factory.close();
+    private static void exibirDadosAluno(Aluno aluno) {
+        System.out.println("ID: " + aluno.getId());
+        System.out.println("Nome: " + aluno.getNome());
+        System.out.println("E-mail: " + aluno.getEmail());
+        System.out.println("CPF: " + aluno.getCpf());
+        System.out.println("Data de Nascimento: " + aluno.getDataNascimento());
+        System.out.println("Naturalidade: " + aluno.getNaturalidade());
+        System.out.println("Endereço: " + aluno.getEndereco());
     }
 }
